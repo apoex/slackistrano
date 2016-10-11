12ec2c9e0762a596018942d91e5cd31b192bf0fb
Stefan Åhman <stefan.ahman@gmail.com>
2016-10-11 11:56:58 +0200

    Format link

--- lib/slackistrano/messaging/apoex.rb
+++ lib/slackistrano/messaging/apoex.rb
@@ -63,7 +63,7 @@ module Slackistrano
         commits.map do |commit|
           tps, name = commit.match(/\[([#\d,\s]*)\]\s*(.+)/).captures
           tps.split(',').map do |tp|
-            "#{tp} - #{name}, <a href='https://apoexab.tpondemand.com/entity/#{tp.gsub('#','')}'>TP</a>"
+            "#{tp} - #{name}, <https://apoexab.tpondemand.com/entity/#{tp.gsub('#','')}'|Link to TP>"
           end
         end.join("\n")
       end
